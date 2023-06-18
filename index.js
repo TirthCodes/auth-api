50cfd978b543a40bb690b69057da6a8d7b68ed58
Tirth Patel <tirthpatelcse@gmail.com>
2023-06-18 18:24:58 +0530

    Add two endpoints and DB connection

--- index.js
+++ index.js
@@ -1,22 +1,85 @@
 import dotenv from 'dotenv';
-import express from 'express'       
+import express from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { PrismaClient } from '@prisma/client';       
 
 dotenv.config()
 
+const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT;
+
+app.use(express.json());
+
+const SECRET_KEY = process.env.SECRET_KEY;
 
 app.get("/", (_req, res) => {
   res.send("Hello World!")
 })
 
-app.get("/signup", (_req, res) => {
-	res.send("Sign Up");
+app.post("/signup", async (req, res) => {
+	const { email, password } = req.body;
+
+  try {
+    // Check if user already exists
+    const existingUser = await prisma.user.findUnique({
+      where: { email }
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ message: 'email already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create the user
+    await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword
+      }
+    });
+
+    res.json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 })
 
-app.get("/login", (_req, res) => {
-	res.send("Login");
-})
+app.post("/login", async (req, res, _next) => {
+	const { email, password } = req.body;
+
+  try {
+    // Check if user exists
+    const user = await prisma.user.findUnique({
+      where: { email }
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    // Compare passwords
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user.id }, SECRET_KEY, {
+      expiresIn: '1h' // Token expires in 1 hour
+    });
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 app.listen(port, () => { 
 	console.log(`Server running on http://localhost:${port} 🚀`);
