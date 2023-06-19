31255da0a849725755daef97f9e9ca15e4269647
Tirth Patel <tirthpatelcse@gmail.com>
2023-06-19 12:55:13 +0530

    Add session and protected routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import session from 'express-session'
 import { PrismaClient } from '@prisma/client';       
 
 dotenv.config()
@@ -11,12 +12,35 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
 
 const SECRET_KEY = process.env.SECRET_KEY;
 
 app.get("/", (_req, res) => {
   res.send("Hello World!")
 })
+
+// JWT authentication middleware
+const authenticateJWT = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'No auth token' });
+  }
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid token' });
+    }
+
+    req.user = user;
+    next();
+  });
+};
 
 app.post("/signup", async (req, res) => {
 	const { email, password } = req.body;
@@ -69,9 +93,12 @@ app.post("/login", async (req, res, _next) => {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
 
+    // Store the user ID in the session
+    req.session.userId = user.id;
+
     // Generate JWT token
     const token = jwt.sign({ userId: user.id }, SECRET_KEY, {
-      expiresIn: '1h' // Token expires in 1 hour
+      expiresIn: '24h'
     });
 
     res.json({ token });
@@ -80,6 +107,40 @@ app.post("/login", async (req, res, _next) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 });
+
+// Logout endpoint
+app.post('/logout', (req, res) => {
+
+  // const { userId } = req.params.id;
+
+  // Destroy the session and clear the user ID
+  req.session.destroy();
+  res.json({ message: 'Logout successful' });
+});
+
+// Protected endpoint with session
+app.get('/protected-session', (req, res) => {
+  // Retrieve the user ID from the session
+  const userId = req.session.userId;
+
+  if (!userId) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  res.json({ message: 'Session protected endpoint accessed successfully' });
+});
+
+// Protected endpoint with JWT authentication
+app.get('/protected-jwt', authenticateJWT, (req, res) => {
+  const userId = req.user.userId;
+
+  if (!userId) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  res.json({ message: 'JWT protected endpoint accessed successfully' });
+});
+
 
 app.listen(port, () => { 
 	console.log(`Server running on http://localhost:${port} 🚀`);
